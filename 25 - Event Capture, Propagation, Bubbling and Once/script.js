65c8645947e44117986b4b009841c65140d4edb2
longo-andrea <longoandrea.dev@gmail.com>
2019-10-23 18:45:54 +0200

    Completed [25/30] challenge of #javascript30

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/script.js
@@ -0,0 +1,10 @@
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+    console.log(this.classList.value);
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
